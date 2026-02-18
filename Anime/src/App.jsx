8905deed031c7d9c666480e54cddd8fd1535c045
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-02-18 18:54:00 +0500

    Anime

--- Anime/src/App.jsx
+++ Anime/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more ok okokokok
+        Click on the Vite and React logos to learn more ok okokokok okokokokook
       </p>
     </>
   )
