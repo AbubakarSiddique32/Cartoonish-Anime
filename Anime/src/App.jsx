786da5fd6e95dfc0bca67926258727a116e4e328
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-02-25 12:57:06 +0500

    Anime

--- Anime/src/App.jsx
+++ Anime/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn ok notihng special more for you let's goood looking awesome look  eery very fantastic sok we will errerererererrereeererererrere rkorkrokrokrork roroororoorororo kok kokokokok kokokokokookokokokokokokokokokokookoko
+        Click on the Vite and React logos to learn ok notihng special more for you let's goood looking awesome look  eery very fantastic soked yes this will we will errerererererrereeererererrere rkorkrokrokrork roroororoorororo kok kokokokok kokokokokookokokokokokokokokokokookoko
       </p>
     </>
   )
