67e0fdd7b7c647d6fd258c280cf303c979114a8a
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-03-16 12:59:18 +0500

    Anime

--- Anime/src/App.jsx
+++ Anime/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and and logo </p>
         <h4>Lets GO and start the hardworking for chasing your goals. and focus on that</h4>
-        <p>ok lets gooo lets have a great evening YES you are right</p>
+        <p>ok lets gooo lets have a great evening YES you are right exactly </p>
      
     </>
   )
