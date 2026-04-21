001a54586ee8907831fdb38058e27ab1bcb3db01
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-04-21 14:55:22 +0500

    Anime

--- Anime/src/Componenets/FirstComponent.jsx
+++ Anime/src/Componenets/FirstComponent.jsx
@@ -30,6 +30,7 @@
          <a href='button'></a>
          <p className='para2'>hey there this is the vs stufio code and im worknig on it </p>
          <p>hey this is second another paragraph so make it unique and atractive</p>
+         <a>Shop Now</a>
           </div>
         </div>
        
