f579d159cbd3b0237f4a65570dba3dc0c0bea409
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-05-02 08:09:27 +0500

    Anime

--- Anime/src/Componenets/FirstComponent.jsx
+++ Anime/src/Componenets/FirstComponent.jsx
@@ -50,7 +50,7 @@
           </div>
           <div className='3rd-div'>
             <p>hey buddy</p>
-            <a>Buy Now</a>
+            <a className='buy-btn'>Buy Now</a>
           </div>
         </div>
         
