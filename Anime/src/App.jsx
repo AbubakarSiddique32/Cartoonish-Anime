b777cff1fc152304baf654f6cae9c40cea36de2a
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-05-07 14:01:56 +0500

    Anime

--- Anime/src/App.jsx
+++ Anime/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FirstComponent from './Componenets/FirstComponent'
+import SecondComponent from './Componenets/SecondComponent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -53,6 +54,7 @@ function App() {
         </div>
         <div className='new-div'></div>
         <FirstComponent />
+        <SecondComponent/>
 
     </>
   )
