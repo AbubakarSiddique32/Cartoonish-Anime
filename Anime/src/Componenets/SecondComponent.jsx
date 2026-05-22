dd4b75b08d0d52f588bc0a41ebdc1dd97e525300
Abubakar Siddique <107239064+AbubakarSiddique32@users.noreply.github.com>
2026-05-22 16:03:36 +0500

    Anime

--- Anime/src/Componenets/SecondComponent.jsx
+++ Anime/src/Componenets/SecondComponent.jsx
@@ -17,7 +17,7 @@ const SecondComponent = () => {
      <div>
      <h1 className=''>Heading</h1>
      <h2 className=''>heading two</h2>
-     <p>paragraph</p>
+     <p>paragraph is the only tag where we can write about body text</p>
      </div>
     </div>
     </div>
